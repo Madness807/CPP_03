bbe0e4afbcc6a6e5fbdd156579da296e9b4ae85c
joterret <jonathan.terrettaz@gmail.com>
2024-02-01 03:44:00 +0100

    Finish ex00, start ex01

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ScavTrap {
+	public:
+		// Constructeurs && destructeur
+		ScavTrap();
+
+		~ScavTrap();
+
+		// Méthodes
+
+
+	private:
+
+};
+
+#endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,12 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: joterrett <joterrett@student.42.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 13:24:25 by joterrett         #+#    #+#             */
-/*   Updated: 2024/01/18 13:24:27 by joterrett        ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
 
+int main()
+{
+	// Declaration
+	std::cout << std::endl;
+	std::cout << "##################################" << std::endl;
+	ClapTrap joshep("joshep");
+	ClapTrap samuel("samuel");
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	//Etas de depart
+	std::cout << "##################################" << std::endl;
+	joshep.getHitPoint();
+	joshep.getEnergyPoint();
+	joshep.getAttackDamage();
+	std::cout << "----------------------------------" << std::endl;
+	samuel.getHitPoint();
+	samuel.getEnergyPoint();
+	samuel.getAttackDamage();
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	//Actions
+	std::cout << "##################################" << std::endl;
+	joshep.attack("samuel");
+	samuel.takeDamage(joshep.getAttackDamage());
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	//Etas apres actions
+	std::cout << "##################################" << std::endl;
+	joshep.getHitPoint();
+	joshep.getEnergyPoint();
+	joshep.getAttackDamage();
+	std::cout << "----------------------------------" << std::endl;
+	samuel.getHitPoint();
+	samuel.getEnergyPoint();
+	samuel.getAttackDamage();
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "##################################" << std::endl;
+
+
+	return (0);
+}
+/*
+When ClapTrack attacks, it causes its target to lose <attack damage> hit points.
+
+When ClapTrap repairs itself, it gets <amount> hit points back. Attacking and repairing cost 1 energy point each.
+
+Of course, ClapTrap can’t do anything if it has no hit points or energy points left.
+5
+*/
